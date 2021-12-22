45581dd368d7dbdac568f51a9bcd26611fb2ae45
dayanm20 <dayanalexismanrique@hotmail.com>
2021-12-22 00:10:01 -0500

    imc calcultion exercise

---
+++ exercise_type_data/exerciste_7.py
@@ -0,0 +1,19 @@
+# Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales.
+
+def imc_calcu(weight:float,height:float)->float:
+    imc=weight/pow(height,2)
+    return imc
+
+def main():
+    weight=float(input("Enter you weight (Kg): "))
+    height=float(input("Enter your height (m): "))
+
+    imc= imc_calcu(weight,height)
+
+    try:
+        return print(f'you imc is: {round(imc,2)}')
+    except ValueError as ve:
+        print(ve)
+
+if __name__ == '__main__':
+    main()
